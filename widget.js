ebbfaa3ffd289712221dd8605303400d6ef47919
Joeli Takala <joeli.takala@helsinki.fi>
2022-10-03 13:05:23 +0300

    Changes to DOM after the Bootstrap 5 update

--- widget.js
+++ widget.js
@@ -3,7 +3,7 @@ var TIETO = TIETO || {};
 
 TIETO = {
     widget: {
-        legendVisible: false,
+        legendVisible: true,
         legendLabels: { 
             false: { 'fi': 'Näytä selite', 'sv': 'Visa förklaring', 'en': 'Show legend' }, 
             true: { 'fi': 'Piilota selite', 'sv': 'Dölj förklaring', 'en': 'Hide legend' }
@@ -15,7 +15,11 @@ TIETO = {
             }
             var legendLabel = TIETO.widget.legendLabels[TIETO.widget.legendVisible][lang];
 
-            $('.concept-info').after(Handlebars.compile($('#tieto-template').html())({'opened': true, 'imgurl': imgUrl, 'images': imgUrls, clang: content_lang, legendLabel: legendLabel}));
+            $('#content-bottom').after(Handlebars.compile($('#tieto-template').html())({'opened': true, 'imgurl': imgUrl, 'images': imgUrls, clang: content_lang, legendLabel: legendLabel}));
+            var $tietoButton = $('#headingTieto > .accordion-button');
+            $tietoButton.on('click', function() {
+
+            });
             $('.panel-body').magnificPopup({
                 delegate: 'a:not(.slick-cloned)',
                 gallery: {
@@ -42,21 +46,9 @@ TIETO = {
                 type: 'iframe'
             });
         },
-        // Flips the icon displayed on the top right corner of the widget header
-        flipChevron: function() {
-            var $glyph = $('#headingTieto > .versal > .glyphicon');
-            if ($glyph.hasClass('glyphicon-chevron-down')) {
-                $glyph.removeClass('glyphicon-chevron-down').addClass('glyphicon-chevron-up');
-                createCookie('TIETO_WIDGET_OPEN', 1);
-            } else {
-                $glyph.removeClass('glyphicon-chevron-up').addClass('glyphicon-chevron-down');
-                createCookie('TIETO_WIDGET_OPEN', 0);
-            }
-        },
         toggleLegend: function() {
             TIETO.widget.legendVisible = !TIETO.widget.legendVisible;
-            $('#collapseTieto > .legend').toggle();
-            $('#headingTieto > .legend').text(TIETO.widget.legendLabels[TIETO.widget.legendVisible][lang]);
+            $('#headingTieto > button').text(TIETO.widget.legendLabels[TIETO.widget.legendVisible][lang]);
         },
         graphsPerUri: { // SHITZU
             "http://urn.fi/URN:NBN:fi:au:tt:t140":[
@@ -554,6 +546,7 @@ $(function() {
         var id = data.uri;
         var openCookie = readCookie('TIETO_WIDGET_OPEN');
         var isOpen = openCookie !== null ? parseInt(openCookie, 10) : 1;
+        var $tietoButton = $('#headingTieto > .accordion-button');
 
         TIETO.widget.render({id: id, opened: isOpen});
         $('.panel-body').slick({
@@ -564,13 +557,14 @@ $(function() {
             prevArrow: '<span class="glyphicon glyphicon-chevron-left"></span>',
             nextArrow: '<span class="glyphicon glyphicon-chevron-right"></span>',
         });
-        $('#headingTieto > .versal > .glyphicon').on('click', function() {
-            TIETO.widget.flipChevron();
-        });
-
-        $('#headingTieto > .legend').on('click', function() {
+        $('#headingTieto > .accordion-button').on('click', function() {
             TIETO.widget.toggleLegend();
-            return false;
+            console.log("Täällä ollaan\n");
+            if ($(this).hasClass('collapsed')) {
+                createCookie('TIETO_WIDGET_OPEN', 0);
+            } else {
+                createCookie('TIETO_WIDGET_OPEN', 1);
+            }
         });
     };
 
